c8330e05f671f4070c41956c4334ef8fdd36412c
Zone <pongthanat.b@gmail.com>
2026-08-07 08:55:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -736,7 +736,7 @@ for _, row in crop_timeline.iterrows():
 
     fig.add_annotation(
         x=midpoint,
-        y=-0.75,
+        y=-0.30,
         xref="x3",
         yref="paper",
         text=(
@@ -771,7 +771,7 @@ fig.update_layout(
         l=20,
         r=20,
         t=50,
-        b=300
+        b=250
     ),
 
     hovermode="closest",
